7baf2ec44b9e25588a0f92dcbc9a848f9d1e4fb4
Mike Pullen <mike.pullen@gmail.com>
2017-10-22 19:48:53 -0500

    Removed brew files not in use

--- brew.sh
+++ brew.sh
@@ -81,17 +81,17 @@ brew install screen
 brew install git
 brew install git-lfs
 brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
+# brew install lua
+# brew install lynx
+# brew install p7zip
+# brew install pigz
+# brew install pv
 brew install rename
-brew install rlwrap
-brew install ssh-copy-id
+# brew install rlwrap
+# brew install ssh-copy-id
 brew install tree
-brew install vbindiff
-brew install zopfli
+# brew install vbindiff
+# brew install zopfli
 
 # Remove outdated versions from the cellar.
 brew cleanup
